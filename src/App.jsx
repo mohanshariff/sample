bdce9445091e1a2b90910b1bf167148f41804886
mohan <sharifmyw@gmail.com>
2023-12-13 21:12:34 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          quidem eius, dolores pariatur hic tempora amet repellat. Expedita
+          harum fuga, qui quae deleniti magni architecto ipsam! Laborum maiores
+          debitis magni.
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          quidem eius, dolores pariatur hic tempora amet repellat. Expedita
+          harum fuga, qui quae deleniti magni architecto ipsam! Laborum maiores
+          debitis magni.
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          quidem eius, dolores pariatur hic tempora amet repellat. Expedita
+          harum fuga, qui quae deleniti magni architecto ipsam! Laborum maiores
+          debitis magni.
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          quidem eius, dolores pariatur hic tempora amet repellat. Expedita
+          harum fuga, qui quae deleniti magni architecto ipsam! Laborum maiores
+          debitis magni.
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          quidem eius, dolores pariatur hic tempora amet repellat. Expedita
+          harum fuga, qui quae deleniti magni architecto ipsam! Laborum maiores
+          debitis magni.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
